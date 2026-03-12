aaf2e9f4c1e1377905458c06e6f31ff197a34eb5
Finbyz <info@finbyz.tech>
2026-03-12 05:27:58 +0000

    undo pinecode fields and number of chunks

--- finbyzai/ai/vectorstores/__init__.py
+++ finbyzai/ai/vectorstores/__init__.py
@@ -6,6 +6,6 @@ when this package (or any submodule like .registry) is imported.
 # Register built-in adapters
 from . import chroma_adapter  # noqa: F401
 # from . import pinecone_adapter  # noqa: F401
-from . import qdrant_adapter  # noqa: F401
+# from . import qdrant_adapter  # noqa: F401
 from . import pinecone_adapter  # noqa: F401
 
